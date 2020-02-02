19b4aa4c706b0ba6be18c625fa85b9f8f767a0e2
Utkarsh Tripathi <utkarshtripathi1602@gmail.com>
2020-02-02 23:14:09 +0530

    changed - utkarsh

--- pattern.java
+++ pattern.java
@@ -1,13 +1,13 @@
 class pattern
-{
-public static void main(String arg[]){
-	StringBuffer s = new StringBuffer("**");
-	for(int i=1;i<=6;i++){
-		System.out.println(s);
-		if(i%2==0){
-			s.append("**");
-}
-}
-}
+	{
+		public static void main(String arg[]){
+			StringBuffer s = new StringBuffer("**");
+			for(int i=1;i<=6;i++){
+				System.out.println(s);
+				if(i%2==0){
+					s.append("**");
+				}
+			}
+		}
 
-}
+	}
